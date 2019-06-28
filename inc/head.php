3a4dffe637d1f16449a60817ea81f0bb873ef4a7
prisillaghadi <52330243+prisillaghadi@users.noreply.github.com>
2019-06-28 23:39:33 +0530

    Create head.php

---
+++ inc/head.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="description for website">
+<meta name="keywords" content="key for website">
+<meta name="author" content="Codechef">
+<title>Data Structure & Algorithms</title>
+
+<meta charset="UTF-8">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,600" rel="stylesheet" type="text/css">
+<link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+<link type="text/css" rel="stylesheet" href="css/style.css">
+<link type="text/css" rel="stylesheet" href="css/responsive.css">
+
+  
+
