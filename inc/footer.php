a1b36d86ecec3036270dec1521fc73b457cb9edf
prisillaghadi <52330243+prisillaghadi@users.noreply.github.com>
2019-06-28 23:39:52 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,76 @@
+<footer class="full">
+
+    
+    <div class="footer-up full">
+             <div class="container">
+                <p>Follow Us</p>
+                <ul class="social-icn-list">
+                    <li><a href=""><img src="img/facebook.png" alt="codechef-imgs"></a></li>
+                    <li><a href=""><img src="img/twitter.png" alt="codechef-imgs"></a></li>
+                    <li><a href=""><img src="img/googleplus.png" alt="codechef-imgs"></a></li>
+                    <li><a href=""><img src="img/instagram.png" alt="codechef-imgs"></a></li>
+                </ul>
+             </div>
+    </div>
+    <div class="footer-down full">
+        <div class="container">
+            <div class="cc-div full">
+                <div class="cc-div-block tbl full">
+                    <div class="cc-div-left tbl-cell">
+                        <div class="mainfordot">
+                            <p>CC DSA</p>
+                            <div class="fourdot">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="cc-div-right tbl-cell">
+                        <ul>
+                            <li><a href=" " class="active">Home</a></li>
+                            <li><a href=" ">About</a></li>
+                            <li><a href=" ">Level</a></li>
+                            <li><a href=" ">Detail</a></li>
+                            <li><a href=" ">Help</a></li>
+
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="code-div full">
+                <div class="left">
+                    <div class="cc-div-block tbl full">
+                        <div class="cc-div-left tbl-cell">
+                            <p>CODECHEF</p>
+
+                        </div>
+                        <div class="cc-div-right tbl-cell">
+                            <ul>
+                                <li><a href=" ">Practise</a></li>
+                                <li><a href=" ">Compete</a></li>
+                                <li><a href=" ">Discuss</a></li>
+                                <li><a href=" ">Collaborate</a></li>
+                                <li><a href=" ">Initiatives</a></li>
+                                <li><a href=" ">Community</a></li>
+
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                 <div class="right">
+                    <span class="pay-text">Payment Methods</span>
+                     <img src="img/payment.png" alt="codechef-imgs">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+   
+   
+</footer>
---
+++ inc/header.php
@@ -0,0 +1,64 @@
+
+<nav class="full">
+    <div class="main-hd-up full">
+        <div class="container">
+            <div class="up-inner full">
+                <div class="up-left">
+                    <ul class="contact-list">
+                        <li>
+                            <span class="cn-list-img"><img  src="img/email.png"></span>
+                            <span class="cn-list-info">ccdsap@codechef.com</span>
+                        </li>
+                        <li>
+                            <span class="cn-list-img"><img src="img/phone.png"></span>
+                            <span class="cn-list-info">+91 93200 60499</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="up-right">
+                    <ul class="log-list">
+                        <li><a href="">Log-in</a></li>
+                        <li><a href="" class="btn">New User</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+    <div class="main-hd full">
+        <div class="container">
+            <div class="main-hd-inner full">
+                <div class="main-hd-left">
+                    <div class="logo-main">
+                        <div class="logo">
+                            <span class="imgdiv"><img src="img/logo.png"></span>
+                            <span class="textdiv"><span class="textdiv-all">( CODECHEF Certifcations )</span><span class="textdiv-all">Data Structure & Algorithms</span> </span>
+                        </div>
+                        <div class="fourdot">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-hd-right">
+                    <input type="checkbox" id="nav" class="hidden"/>
+                      <label for="nav" class="nav-open"><i></i><i></i><i></i></label>
+                      <div class="nav-container">
+                        <ul>
+                          <li><a href="#" class="active">Home</a></li>
+                          <li><a href="#">About</a></li>
+                          <li><a href="#">Levels</a></li>
+                          <li><a href="#">Details</a></li>
+                          <li><a href="#">Help</a></li>
+                        </ul>
+                      </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  
+</nav>
+    
